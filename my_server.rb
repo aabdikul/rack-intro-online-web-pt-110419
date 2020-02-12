0ec2409ce0bb33457958e67d213848c5bbcb32cd
Aida Abdikulova <abdikulova@gmail.com>
2020-02-12 06:04:12 +0000

    Automatically backed up by Learn

--- my_server.rb
+++ my_server.rb
@@ -6,4 +6,8 @@ class MyServer
   def pretty_response
     (Time.now.to_i % 2).zero? ?  ["<em>Hello</em>"] : ["<strong>Hello</strong>"]
   end
+
+  def Hello
+    puts "Hello, my name is Aida"
+  end
 end
